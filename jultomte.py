e8e564c3fb7f3a66584b0adc8580ee2457163531
Alexander R <alexanderrehnman2004@gmail.com>
2021-12-15 09:43:55 +0100

    lektionen slut igen

--- jultomte.py
+++ jultomte.py
@@ -1,17 +1,24 @@
 def main():
 
     öppna_skapa= int(input("Ifall du vill skapa önskelista tryck 1. Ifall du vill öppna önskelista tryck 2: "))
+    
+    global önskelista
+
 
     if öppna_skapa==1:
         önskelista = input("vad ska önskelistan heta?: ")
-        namn = input("Vad heter personen?: ")
-        antal = int(input("Hur många grejer vill du skriva in?: "))
+        with open( önskelista +".txt", "w", encoding="utf8") as önskelista:
+            print("mata in din önskelista")
+            namn = input("Vad heter personen?: ")
 
-        for saker in antal[0,antal]:
+            while True:
+        
+                present = input("skriv in dina presenter. avsluta genom att trycka h: ")
+                if "h" in present:
+                    break
+                    
+                else:
+                    önskelista.write(f"{present}\n")
 
-
-
-         with open("önskelista.txt", "w", encoding="utf8") as önskelista:
-             önsk= önskelista.readlines()
 
 main()
